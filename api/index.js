2179456b385cfc30fd93241317655e18a6b8d12d
IgenJa <schneppzeteny@gmail.com>
2026-05-08 17:03:23 +0200

    vercel fix

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 const { app } = require("../server.js");
 
-module.exports = (req, res) => app(req, res);
+module.exports = (req, res) => {
+  // Vercel rewrite esetén az eredeti útvonalat query-ben adjuk át: ?path=/posts/123
+  // Expressnek visszaadjuk az eredeti URL-t, hogy a route-ok illeszkedjenek.
+  try {
+    const url = new URL(req.url, "http://localhost");
+    const originalPath = url.searchParams.get("path");
+    if (originalPath) {
+      req.url = originalPath;
+    }
+  } catch {
+    // ignore
+  }
+  return app(req, res);
+};
 
